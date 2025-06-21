405dc5847c863d745082a709ff2e8a3278deceb0
Meha <mixiadami@gmail.com>
2025-06-21 23:23:40 +0200

    ADD MIN VALUE

--- script.js
+++ script.js
@@ -5,11 +5,16 @@
 
 const calcTempAmplitude = function (temps) {
   let max = temps[0];
+  let min = temps[0];
   for (let i = 0; i < temps.length; i++) {
     if (temps[i] > max) {
       max = temps[i];
     }
+    if (temps[0] < min) {
+      min = temps[0];
+    }
   }
   console.log(max);
+  console.log(min);
 };
-calcTempAmplitude([3, 7, 4]);
+calcTempAmplitude([1, 3, 7, 4, 23]);
