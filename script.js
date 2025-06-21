52297aae3b03bc827e8be217b19a81196eaa46f7
Meha <mixiadami@gmail.com>
2025-06-21 11:46:23 +0200

    CREATE MAX VALUE

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+// MAX VALUE
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    if (temps[i] > max) {
+      max = temps[i];
+    }
+  }
+  console.log(max);
+};
+calcTempAmplitude([3, 7, 4]);
